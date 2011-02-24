2a1efa9d62121ee3961d11633bafc0d0fbf53428
Sergio Oliveira Jr <sergio.oliveira.jr@gmail.com>
2011-02-24 14:10:11 -0800

    better method to load all files from gem's lib

--- lib/options-lib.rb
+++ lib/options-lib.rb
@@ -1,6 +1,3 @@
 # require all files from options-lib
-require 'options-lib/helpers'
-require 'options-lib/option'
-require 'options-lib/option_quote'
-require 'options-lib/yahoo_crawler'
+Dir[File.dirname(__FILE__) + "/options-lib/**/*.rb"].each { |file| require file }
 
